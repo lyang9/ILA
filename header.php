ba43e214f1e0cf04a7e723732abc6fc58c852603
lyang9 <lyang9@buffalo.edu>
2016-07-15 19:23:33 -0400

    added ILA.jpg

---
+++ header.php
@@ -0,0 +1,51 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<style type="text/css">
+	.footer
+	{
+      text-align: left;
+      position: absolute;
+  		bottom: 0;
+      width: 100%;
+  		/* Set the fixed height of the footer here */
+  		height: 100px;
+  		background-color: #00CCFF;
+
+  }
+</style>
+
+
+
+<html>
+<ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="index.php">Home</a></li>
+  <li role="presentation"><a href="about.php">About ILA</a></li>
+  <li class="dropdown">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Members 
+  <span class="caret"></span></a>
+  <ul class="dropdown-menu">
+  <li><a href="#">Executive Boards</a></li>
+  <li><a href="#">Language Directors</a></li>
+  <li><a href="#">Mentors</a></li>
+  </ul>
+  </li>
+  <li role="presentation"><a href="#">Events/Workshops</a></li>
+  <li role="presentation"><a href="contact.php">Contact Us</a></li>
+</ul>
+
+
+
+
+
+
+
+
+
+
